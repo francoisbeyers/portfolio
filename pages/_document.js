1cc31c974e049533ec4a8c802524624f02a05155
Francois <francois.beyers@gmail.com>
2023-02-07 09:19:30 +0000

    added gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   return (
     <Html>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"

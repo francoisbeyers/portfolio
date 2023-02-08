07c2ee5dfe9a943755364c18c4b58fa6b5a5d231
Francois <francois.beyers@gmail.com>
2023-02-08 14:14:52 +0000

    styling

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home() {
                 <a
                   href="/public/Resume-François-Beyers.pdf"
                   download={"Resume-François-Beyers.pdf"}
-                  className="px-4 py-2 text-sm font-medium border-2 rounded-full shadow-lg border-dark-grey text-dark-grey hover:bg-dark-grey hover:text-white hover:duration-500"
+                  className="hidden px-4 py-2 text-sm font-medium border-2 rounded-full shadow-lg md:block border-dark-grey text-dark-grey hover:bg-dark-grey hover:text-white hover:duration-500"
                 >
                   Resume
                 </a>
@@ -78,63 +78,74 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="p-10 py-10 text-center">
-            <h1 className="py-2 text-6xl font-thin uppercase text-dark-grey font-amiri md:text-9xl">
-              Francois Beyers
-            </h1>
-            <h2 className="py-4 text-3xl text-dar-grey md:text-5xl">
-              Experienced Web Developer and SEO Expert for Hire
-            </h2>
-            <p className="max-w-xl py-5 mx-auto leading-8 text-gray-600 text-extralight md:text-extralight">
-              Transform Your Online Presence with My Personalised Web
-              Development and SEO Services. As a friendly and dedicated
-              freelancer, I take pride in crafting custom websites that not only
-              look great, but also perform well in search engines. Let&apos;s
-              work together to bring your web vision to life!
-            </p>
-            <div className="flex justify-center gap-16 pt-10 text-5xl">
-              <Link href="https://www.linkedin.com/in/francois-beyers/">
-                <a
-                  className="text-opacity-25 hover:text-opacity-100 text-orange hover:duration-500"
-                  aria-label="Link that will take you LinkedIn profile"
+
+          <div className="flex flex-col p-10 py-10 text-center md:flex-row">
+            <div className="md:text-left md:w-1/2 md:mx-auto">
+              <h1 className="py-2 text-sm md:text-lg text-dark-grey font-amiri">
+                hello, I'm{" "}
+              </h1>
+              <h2 className="py-2 text-5xl font-thin uppercase text-dark-grey font-amiri md:text-9xl">
+                Francois Beyers
+              </h2>
+              <h3 className="py-4 text-3xl text-dar-grey md:text-5xl">
+                Experienced Web Developer and SEO for Hire
+              </h3>
+              <p className="max-w-xl py-5 leading-8 text-gray-600 text-extralight md:text-extralight">
+                Transform Your Online Presence with My Personalised Web
+                Development and SEO Services. As a friendly and dedicated
+                freelancer, I take pride in crafting custom websites that not
+                only look great, but also perform well in search engines.
+                Let&apos;s work together to bring your web vision to life!
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-16 pt-10 text-5xl md:w-1/2 md:mx-auto">
+              <div className="flex gap-8">
+                <Link href="https://www.linkedin.com/in/francois-beyers/">
+                  <a
+                    className="text-opacity-25 hover:text-opacity-100 hover:drop-shadow-lg text-orange hover:duration-500"
+                    aria-label="Link that will take you LinkedIn profile"
+                  >
+                    <AiFillLinkedin />
+                  </a>
+                </Link>
+                <Link href="https://twitter.com/BeyersFrancois">
+                  <a
+                    className="text-opacity-25 hover:text-opacity-100 hover:drop-shadow-lg text-orange hover:duration-500"
+                    aria-label="Link that will take you Twitter profile"
+                  >
+                    <AiFillTwitterCircle />
+                  </a>
+                </Link>
+                <Link href="https://www.instagram.com/francoisbeyers/">
+                  <a
+                    className="text-opacity-25 hover:text-opacity-100 hover:drop-shadow-lg text-orange hover:duration-500"
+                    aria-label="Link that will take you Instagram profile"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link
+                  href="mailto:francois@beyers.tech?subject=Hi Francois, I'd like to work with you"
+                  aria-label="Link that will take you to contact page"
                 >
-                  <AiFillLinkedin />
-                </a>
-              </Link>
-              <Link href="https://twitter.com/BeyersFrancois">
-                <a
-                  className="text-opacity-25 hover:text-opacity-100 text-orange hover:duration-500"
-                  aria-label="Link that will take you Twitter profile"
-                >
-                  <AiFillTwitterCircle />
-                </a>
-              </Link>
-              <Link href="https://www.instagram.com/francoisbeyers/">
-                <a
-                  className="text-opacity-25 hover:text-opacity-100 text-orange hover:duration-500"
-                  aria-label="Link that will take you Instagram profile"
-                >
-                  <AiFillInstagram />
-                </a>
-              </Link>
+                  <a className="flex justify-center my-10">
+                    <button className="px-6 py-2 text-xl font-medium border-2 rounded-full shadow-lg md:px-10 md:py-4 text-orange border-orange hover:bg-orange hover:duration-500 hover:text-dark-grey ">
+                      Let&apos;s work together
+                    </button>
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
-
-          <Link
-            href="mailto:francois@beyers.tech?subject=Hi Francois, I'd like to work with you"
-            aria-label="Link that will take you to contact page"
-          >
-            <a className="flex justify-center my-10">
-              <button className="px-10 py-4 text-xl font-medium border-2 rounded-full shadow-lg text-orange border-orange hover:bg-orange hover:duration-300 hover:text-dark-grey ">
-                Let&apos;s work together
-              </button>
-            </a>
-          </Link>
         </section>
         <section>
           <div>
             <div className="py-10 text-center">
-              <h3 className="py-1 text-gray-800 ">Services I offer</h3>
+              <h3 className="py-1 text-gray-800 font-amiri ">
+                Services I offer
+              </h3>
             </div>
             <div className="justify-center mt-8 mb-8 space-y-8 md:columns-2 md:flex md:gap-12 md:space-y-0">
               <div className="space-y-8">
@@ -257,7 +268,7 @@ export default function Home() {
                 </p>
               </div>
               <a
-                className="flex items-center justify-center px-4 py-2 text-4xl uppercase text-gray-800/50 hover:text-gray-800 font-amiri"
+                className="flex items-center justify-center px-4 py-2 text-4xl text-gray-800 uppercase hover:text-black hover:duration-500 font-amiri"
                 href="mailto:francois@beyers.tech"
               >
                 Send an
